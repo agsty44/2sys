42c03df3be9cf0a45365654ce52463c019c6ed9b
agsty44 <alexandiaingaming@gmail.com>
2025-07-10 21:51:52 +0100

    added cookie setting/unsetting functions to universal.php

--- universal.php
+++ universal.php
@@ -9,4 +9,24 @@ function sanitiseStrings($data) {
     $data = htmlspecialchars($data);
     return $data;
 }
+
+function establishLoginCookies() {
+    global $user, $hashedPass;
+
+    //At this point, we should also commit the user's username and password to their cookies, 
+    //ofc hashing the password for security.
+
+    setcookie('dXNlcm5hbWVDb29raWU=', $user, time() + (86400 * 7), '/');
+    //that is 'usernameCookie' in b64.
+
+    //set the hashed pass here (we already have it:)
+    setcookie('cGFzc3dvcmRDb29raWU=', $hashedPass, time() + (86400 * 7), '/');
+    //cookie name is 'passwordCookie' in b64
+    //yes i know that's nowhere near secure but it beats storing it under the name 'passwordCookie'
+}
+
+function unsetCookies() {
+    setcookie('dXNlcm5hbWVDb29raWU=', '', time() -1, '/');
+    setcookie('cGFzc3dvcmRDb29raWU=', '', time() -1, '/');
+}
 ?>
